d746406ad5f498b7b9ddebcf1a62986bf3a026d5
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-06-20 23:55:29 -0400

    Change scrapeTest URL, add element selectors

--- tests/scrapeTest.js
+++ tests/scrapeTest.js
@@ -2,7 +2,8 @@ var cheerio = require("cheerio");
 var request = require("request");
 
 
-const scrapeURL = "https://www.npr.org/sections/news/";
+// const scrapeURL = "https://www.npr.org/sections/news/";
+const scrapeURL = "https://blog.mozilla.org/";
 
 // First, tell the console what server.js is doing
 console.log(`Scraping ${scrapeURL}`);
@@ -14,7 +15,7 @@ request(scrapeURL, function(error, response, html) {
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
   var $ = cheerio.load(html);
 
-  console.log(html.length);
+  // console.log(html.length);
   
 
   // An empty array to save the data that we'll scrape
@@ -22,19 +23,25 @@ request(scrapeURL, function(error, response, html) {
 
   // With cheerio, find each p-tag with the "title" class
   // (i: iterator. element: the current element)
-  $("h2.title").each(function(i, element) {
+  $("article.post").each(function(i, element) {
+    let title = $('h2.entry-title', element).text().trim();
+    // console.log('title:', title)
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
-
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-
+    let link = $('a.go', element).attr("href").trim();
+    // console.log('link:', link);
+    
+    let author = $('address.vcard', element).text().trim();
+    // console.log('author:', author);
+    
+    let summary = $('div.entry-summary', element).text().trim();
+    // console.log('summary:', summary);
+    
     // Save these results in an object that we'll push into the results array we defined earlier
     results.push({
-      title: title,
-      link: link
+      title,
+      link,
+      author,
+      summary
     });
   });
 
