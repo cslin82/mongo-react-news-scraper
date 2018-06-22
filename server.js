6bc107d64a73f74b95bc81567487af761072e4cb
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-06-22 02:30:18 -0400

    Cleanup server.js

--- server.js
+++ server.js
@@ -2,23 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
 const exphbs  = require('express-handlebars');
-
-// var cheerio = require("cheerio");
-// var request = require("request");
 
 var PORT = process.env.PORT || 8080;
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-// mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-
-
 
 // Initialize Express
 var app = express();
@@ -27,10 +19,6 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-var indexRouter = require('./routes/index');
-app.use('/', indexRouter);
-
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
@@ -38,10 +26,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-// mongoose.connect("mongodb://localhost/newsdb");
-
 // Routes
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
 // Start the server
 app.listen(PORT, function() {
