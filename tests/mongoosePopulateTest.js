367cfce15472bc429f35f420e2717f43715b0b83
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-06-22 23:09:31 -0400

    Add note test stubs

---
+++ tests/mongoosePopulateTest.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Article = require('../models/articleModel')
+const Note = require('../models/noteModel')
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI)
+.then(
+    () => { console.log('connected to ' + MONGODB_URI);
+     },
+    err => { throw err }
+)
+.then(
+    () => {
+        Article.findById("5b2db52694c08956eb6b64a1")
+
+        .populate('notes')
+        .then(function(dbArticle) {
+            console.log(JSON.stringify(dbArticle, '', 2))
+        })
+        
+    })
+
