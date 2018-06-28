3184f131dadd1d8e8cdd6f13f2310caa7b0a6a5c
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-06-28 01:29:43 -0400

    Cleanup error handler

--- server.js
+++ server.js
@@ -45,9 +45,7 @@ mongoose.connect(MONGODB_URI)
     app.listen(PORT, function () {
       console.log("App running on port " + PORT + "; go to http://localhost:" + PORT + "/");
     });
-  }
-  )
-  .catch(
-    (err) => { throw err }
-  );
-
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
