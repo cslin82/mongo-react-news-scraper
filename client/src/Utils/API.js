ef6ee41fe5256179c7137917b849813d71b24e07
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-09-23 01:54:31 -0400

    Add API handlers

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
 export default {
-  getPosts: function () {
-    return axios.get('/api/posts');
+  getPosts: function() {
+    return axios.get('/api/articles');
   },
-  scrape: function () {
+  scrape: function() {
     return axios.get('/api/scrape');
+  },
+  postNote: function(articleID) {
+    return axios.post('/api/notes/' + articleID);
+  },
+  toggleStory: function(articleID) {
+    return axios.get('/api/toggleStory/' + articleID);
+  },
+  deleteNote: function(noteID) {
+    return axios.delete('/api/notes/' + noteID);
   }
 };
