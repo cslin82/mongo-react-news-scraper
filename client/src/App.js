fca10cb47139759a497c7e0f1bc7c2f0528b794c
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-09-26 01:24:48 -0400

    Start semantic-ui-react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Container, Grid, Segment, Header } from 'semantic-ui-react';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <Router>
           <Switch>
-            <Route exact path='/saved' />
-            <Route exact path='/help' />
-            <Route exact path='/' />
+            <Route exact path="/saved" />
+            <Route exact path="/help" />
+            <Route exact path="/" />
             {/* <Route /> */}
 
-
-          <header className="App-header">
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+            <header className="App-header">
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
           </Switch>
         </Router>
-      </div>
+      </Container>
     );
   }
 }
