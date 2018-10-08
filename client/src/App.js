a2697fece60ba90423ee42f57042162e40ec6fa6
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-10-08 19:26:23 -0400

    Fix formatting, add footer

--- client/src/App.js
+++ client/src/App.js
@@ -30,8 +30,9 @@ class App extends Component {
         <Router>
           <div>
             <Navbar color="light" light expand="md">
-              <Nav className="ml-auto" navbar>
+              <Nav navbar>
                 <NavItem>
+                  {/* TODO fix a tag nesting */}
                   <NavLink>
                     <RRNavLink to="/">Home</RRNavLink>
                   </NavLink>
@@ -48,21 +49,36 @@ class App extends Component {
                 </NavItem>
               </Nav>
             </Navbar>
-            <Jumbotron>
-              <h1 className="display-4">Mozilla Blog article scraper</h1>
-              <p className="lead">
-                This Node.js-Express-MongoDB-Mongoose app scrapes blog posts from Mozilla into a Mongo database. It lets
-                you save and make notes on them.
-              </p>
-              <a className="btn btn-primary btn-block action-scrape" href="/scrape" role="button">
-                Scrape new stories
-              </a>
-            </Jumbotron>
-            <Switch>
-              <Route exact path="/saved" component={Saved} />
-              <Route exact path="/help" component={Help} />
-              <Route exact path="/" render={props => <Home {...props} articles={this.state.articles} />} />
-            </Switch>
+            <div className="container">
+              <Jumbotron>
+                <h1 className="display-4">Mozilla Blog article scraper</h1>
+                <p className="lead">
+                  This Node.js-Express-MongoDB-Mongoose app scrapes blog posts from Mozilla into a Mongo database. It
+                  lets you save and make notes on them.
+                </p>
+                <a className="btn btn-primary btn-block action-scrape" href="/scrape" role="button">
+                  Scrape new stories
+                </a>
+              </Jumbotron>
+              <Switch>
+                <Route exact path="/saved" component={Saved} />
+                <Route exact path="/help" component={Help} />
+                <Route exact path="/" render={props => <Home {...props} articles={this.state.articles} />} />
+              </Switch>
+            </div>
+
+            <footer className="container py-5">
+              <div className="row">
+                <div className="col-md">
+                  <hr />
+                  <p className="d-block bg-secondary mb-3 p-1 text-light text-center rounded">All caught up!</p>
+                  <p className="d-block mb-3 text-center">
+                    <i className="fab fa-github" />
+                    <a href="https://github.com/cslin82/mongo-react-news-scraper"> @cslin82/mongo-react-news-scraper</a> on GitHub
+                  </p>
+                </div>
+              </div>
+            </footer>
           </div>
         </Router>
       </div>
